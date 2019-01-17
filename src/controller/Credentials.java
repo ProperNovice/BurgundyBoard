a9ae2e2b60767db53a76f6191e5462c7ff0b1f2f
ProperNovice <ProperNovice@users.noreply.github.com>
2019-01-17 12:24:02 +0200

    Update Credentials.java

--- src/controller/Credentials.java
+++ src/controller/Credentials.java
@@ -2,8 +2,6 @@ package controller;
 
 public class Credentials {
 
-	// Frame (1366, 788)
-
 	public static Pair DimensionsInsets, DimensionsGapBetweenBorders, DimensionsBoard, DimensionsTile,
 			DimensionsDiceAreaInSpace, DimensionsGoods, DimensionsFrame;
 	public static Pair CoordinatesTextPanel, CoordinatesBoard;
@@ -13,15 +11,17 @@ public class Credentials {
 
 		boardScale = 0.8;
 
+//		DimensionsFrame = new Pair(1366, 788);
 		DimensionsInsets = new Pair(7, 29);
 		DimensionsGapBetweenBorders = new Pair(20, 20);
+		CoordinatesTextPanel = new Pair(50, 50);
+
 		DimensionsBoard = new Pair(1121, 781);
 		DimensionsTile = new Pair(70, 80);
 		DimensionsDiceAreaInSpace = new Pair(46, 46);
 		DimensionsGoods = new Pair(60, 60);
 		DimensionsFrame = new Pair(1420, Math.ceil(DimensionsBoard.y * boardScale + 2 * DimensionsGapBetweenBorders.y));
 
-		CoordinatesTextPanel = new Pair(50, 50);
 		CoordinatesBoard = new Pair(DimensionsGapBetweenBorders.x, DimensionsGapBetweenBorders.y);
 
 		textHeight = 50;
