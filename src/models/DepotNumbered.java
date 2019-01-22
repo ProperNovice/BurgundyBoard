11862cde422cc64dd225597e38212e7d4bc46021
ProperNovice <ProperNovice@users.noreply.github.com>
2019-01-22 11:47:28 +0200

    Update DepotNumbered.java

--- src/models/DepotNumbered.java
+++ src/models/DepotNumbered.java
@@ -1,9 +1,34 @@
 package models;
 
-public class DepotNumbered {
-	
-	public DepotNumbered() {
+import enums.TileTypeEnum;
+import tiles.Tile;
+import utils.ArrayList;
 
+public class DepotNumbered {
+
+	private int depotNumber;
+	private ArrayList<TileTypeEnum> tilesTypeEnum = new ArrayList<>();
+	private ArrayList<Tile> tiles = new ArrayList<>();
+	private ArrayList<Goods> goods = new ArrayList<>();
+
+	public DepotNumbered(int depotNumber, TileTypeEnum tileTypeEnumFirst, TileTypeEnum tileTypeEnumSecond) {
+
+	}
+
+	public TileTypeEnum getTileTypeEnumFirst() {
+		return this.tilesTypeEnum.getFirst();
+	}
+
+	public TileTypeEnum getTileTypeEnumSecond() {
+		return this.tilesTypeEnum.getLast();
+	}
+
+	public boolean containsTile(Tile tile) {
+		return this.tiles.contains(tile);
+	}
+	
+	public boolean containsGoods(Goods goods) {
+		return this.goods.contains(goods);
 	}
 
 }
